b284d918483e0156487c8202c0c325b54b844bd3
Юрій <yurijlitviniii@gmail.com>
2022-08-14 17:01:05 +0300

    app.js: Create get() and Render 'home', 'login' & 'register' pages; Create db connection; Create Schemf & model

--- app.js
+++ app.js
@@ -2,16 +2,35 @@
  const express = require("express");
  const bodyParser = require("body-parser");
  const ejs = require("ejs");
+ const mongoose = require("mongoose");
 
  const app = express();
 
 app.use(express.static("public"));
-app.use("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 
+app.get("/", function(req, res) {
+  res.render("home");
+});
 
+app.get("/login", function(req, res) {
+  res.render("login");
+});
 
+app.get("/register", function(req, res) {
+  res.render("register");
+});
 
 
 
