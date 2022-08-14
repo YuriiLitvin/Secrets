dfff075d0c2125e287c3ec3ea6a851c87c3840f2
Юрій <yurijlitviniii@gmail.com>
2022-08-15 00:02:58 +0300

    app.js: Configurate 'express-session', 'passport' & 'passport-local-mongoose'

--- app.js
+++ app.js
@@ -4,14 +4,24 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(session({
+  secret: "Thisisourlittlesecret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -20,8 +30,13 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/", function(req, res) {
@@ -40,42 +55,15 @@ app.post("/register", function(req, res) {
   const userEmail = req.body.username;
   const password = req.body.password;
 
-  bcrypt.hash(password, saltRounds, function(err, hash) {
-    const newUser = new User ({
-      email: userEmail,
-      password: hash
-    });
 
-    newUser.save(function(err) {
-      if (!err) {
-        console.log("Registration is completed successfully.");
-        res.render("secrets");
-      } else {
-        console.log(err);
-      }
-    });
-  });
 });
 
 app.post("/login", function(req, res) {
   const userEmail = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: userEmail}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("secrets");
-          } else {
-            console.log(err);
-          }
-        });
-      }
-    }
-  });
+
+
 });
 
 app.listen(3000, function() {
