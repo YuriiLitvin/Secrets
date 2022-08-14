30067844e89743843b2f0251507ebd8be3075b4b
Юрій <yurijlitviniii@gmail.com>
2022-08-14 16:21:41 +0300

    app.js: Create boiler plate; npm init; Install express, ejs, body-parser packages

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-//jshint esversion:6
+
+ const express = require("express");
+ const bodyParser = require("body-parser");
+ const ejs = require("ejs");
+
+ const app = express();
+
+app.use(express.static("public"));
+app.use("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
