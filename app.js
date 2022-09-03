1c7b2dc93a21855c2afa5b6c77ab916f9b00c832
unknown <yurijlitviniii@gmail.com>
2022-09-03 21:28:05 +0300

    app.js: Remove from google and facebook callbackURLs 'http://localhost:3000'

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets",
+  callbackURL: "/auth/google/secrets",
 },
   function(accessToken, refreshToken, profile, cb) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
@@ -67,7 +67,7 @@ passport.use(new GoogleStrategy({
 passport.use(new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: "http://localhost:3000/oauth2/redirect/facebook",
+  callbackURL: "/oauth2/redirect/facebook",
 },
   function(accessToke, refreshToken, profile, cb) {
     User.findOrCreate({ facebookId: profile.id }, function(err, user) {
